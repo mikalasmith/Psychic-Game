3297fc20adc6a028203cc460a192eb105a86d3a8
Mikala Smith <mikalasmith@mikalas-air.home>
2018-03-31 18:55:32 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,23 @@
-// Create an array of letter choices
-var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                   "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x",
-                   "y", "z"];
-    // console.log(compChoice); 
+// // Create an array of letter choices
+// var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+//                    "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x",
+//                    "y", "z"];
+//     // console.log(compChoice); 
 
-// Let the computer choose random letters
-var randomLetter = compChoice [Math.floor(Math.random() * compChoice.length)] 
-    // console.log(randomLetter); 
+// // Let the computer choose random letters
+// var randomLetter = compChoice [Math.floor(Math.random() * compChoice.length)] 
+//     // console.log(randomLetter); 
 
-// Press any key to start the game
-document.onkeyup = function(){
-    var userGuess = event.key;
-    console.log(this);
+// // Press any key to start the game
+// document.onkeyup = function(){
+//     var userGuess = event.key;
+    
 
-}
+// }
 
 
-var wins = 0;
-var losses = 0;
-var numGuesses= 5;
-var guessChoices = [];
+// var wins = 0;
+// var losses = 0;
+// var numGuesses= 5;
+// var guessChoices = [];
      
